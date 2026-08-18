2ff2f98582a8e9352a6881469a3f0cebf6802d9d
shusei wakazono <shusei.wakazono@thedott.io>
2026-08-18 13:27:56 +0900

    #10操作ログを表示するを完了しました

--- intern-book-app/src/app/app.component.ts
+++ intern-book-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MessageComponent } from './components/message/message.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent, MessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
